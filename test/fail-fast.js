e8360d2032486ad9e444d3e515b4624c84b0c9ab
Ben Page <ben.page@openreign.com>
2016-08-07 22:43:20 -0500

    fixed fail fast

---
+++ test/fail-fast.js
@@ -0,0 +1,24 @@
+'use strict';
+const Promise = require('bluebird');
+Promise.config({
+    warnings: true,
+    longStackTraces: true
+});
+const Suite = require('../lib/suite');
+
+const suite = new Suite('fail fast', { failFast: true });
+
+suite.test('pass', t => {
+    t.equal(1, 1);
+});
+
+suite.test('fail', t => {
+    t.equal(1, 2);
+});
+
+suite.test('stop', t => {
+    return Promise.delay(100)
+        .then(() => {
+            t.equal(1, 1);
+        });
+});
