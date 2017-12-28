17eed6a4e425cacacb016f3dd7469b5993a8a22a
Ben Page <ben.page@openreign.com>
2017-12-28 12:13:09 -0600

    before and after hooks working stopping running tests working fixed bug causing test to run twice

--- test/serial.js
+++ test/serial.js
@@ -1,26 +1,32 @@
 'use strict';
 const Suite = require('../lib/Suite');
+const t = require('../lib/assert');
+const promise = require('../lib/promise');
 
 const suite = new Suite('asynchronous & serial');
 
 let count = 1;
 
+suite.before(() => {
+    t.equal(count, 1);
+});
+
 suite.test('asynchronous test', () => {
-    return Promise.delay(10)
+    return promise.delay(10)
         .then(() => {
             t.equal(count++, 3);
         });
 });
 
 suite.serial.test('serial 1', () => {
-    return Promise.delay(300)
+    return promise.delay(300)
         .then(() => {
             t.equal(count++, 1);
         });
 });
 
 suite.serial.test('serial 2', () => {
-    return Promise.delay(100)
+    return promise.delay(100)
         .then(() => {
             t.equal(count++, 2);
         });
