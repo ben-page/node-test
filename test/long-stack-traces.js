2c22b25a43b0d12d4c43c340e03d6c64cdcc9cdb
Ben Page <ben.page@openreign.com>
2017-12-28 18:44:45 -0600

    long stack traces test passing

--- test/long-stack-traces.js
+++ test/long-stack-traces.js
@@ -1,53 +1,54 @@
 'use strict';
-const Promise = require('bluebird');
 const Suite = require('../lib/Suite');
+const t = require('../lib/assert');
 
 const suite = new Suite('fail fast');
 
-suite.test('callback', (t, done) => {
-    function thing(cb) {
-        setTimeout(() => {
-            cb()
-        }, 10);
+suite.test('callback', () => {
+    function thing() {
+        setImmediate(() => {
+            setTimeout(() => {
+                throw new Error('error');
+            }, 10);
+        });
     }
-    
-    function thing2(cb) {
-        setTimeout(() => {
-            cb(new Error('error'))
-        }, 10);
-    }
-    
-    thing(t.async(err => {
-        thing2(t.async());
-    }));
+
+    thing();
 },
-(err, t) => {
+err => {
     t.equals(err.stack, `Error: error
-    at Timeout.setTimeout (C:\\projects\\node-test\\test\\long-stack-traces.js:16:16)
-From previous event:
-    at Assert.t.async.err (C:\\projects\\node-test\\test\\long-stack-traces.js:21:18)
-    at Timeout.setTimeout (C:\\projects\\node-test\\test\\long-stack-traces.js:10:13)
-From previous event:
-    at suite.test (C:\\projects\\node-test\\test\\long-stack-traces.js:20:13)`);
+    at Timeout.setTimeout [as _onTimeout] (C:\\projects\\node-test\\test\\long-stack-traces.js:11:23)
+Async call from Timeout:
+    at Immediate.setImmediate (C:\\projects\\node-test\\test\\long-stack-traces.js:10:13)
+Async call from Immediate:
+    at thing (C:\\projects\\node-test\\test\\long-stack-traces.js:9:9)
+    at suite.test (C:\\projects\\node-test\\test\\long-stack-traces.js:16:5)
+Async call from node-test.Test:
+    at Object.<anonymous> (C:\\projects\\node-test\\test\\long-stack-traces.js:7:7)`);
 });
 
 suite.test('promises', () => {
-    function thing3() {
-        return Promise.resolve()
-            .then(() => {
-                throw new Error('error');
+    function thing() {
+        return new Promise((resolve, reject) => {
+            setImmediate(() => {
+                setTimeout(() => {
+                    reject(new Error('error'));
+                }, 100);
             });
+        });
     }
-    
-    return thing3();
+
+    thing();
 },
-(err, t) => {
-    console.log(err.stack);
+err => {
     t.equals(err.stack, `Error: error
-    at Promise.resolve.then (C:\\projects\\node-test\\test\\long-stack-traces.js:38:23)
-From previous event:
-    at thing3 (C:\\projects\\node-test\\test\\long-stack-traces.js:37:14)
-    at suite.test.t (C:\\projects\\node-test\\test\\long-stack-traces.js:42:12)
-From previous event:
-    at Object.<anonymous> (C:\\projects\\node-test\\test\\long-stack-traces.js:34:7)`);
+    at Timeout.setTimeout [as _onTimeout] (C:\\projects\\node-test\\test\\long-stack-traces.js:35:28)
+Async call from Timeout:
+    at Immediate.setImmediate (C:\\projects\\node-test\\test\\long-stack-traces.js:34:17)
+Async call from Immediate:
+    at Promise (C:\\projects\\node-test\\test\\long-stack-traces.js:33:13)
+    at thing (C:\\projects\\node-test\\test\\long-stack-traces.js:32:16)
+    at suite.test (C:\\projects\\node-test\\test\\long-stack-traces.js:41:5)
+Async call from node-test.Test:
+    at Object.<anonymous> (C:\\projects\\node-test\\test\\long-stack-traces.js:30:7)`);
 });
